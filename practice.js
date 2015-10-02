8e63b46de06c6afac25b50adaa737b1cdf4065c5
shelbybjensen <shelbybjensen@gmail.com>
2015-10-01 20:38:38 -0600

    3rd problem

--- practice.js
+++ practice.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+  inner();
 
 
 
@@ -32,7 +32,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+var dialing = callFriend();
+dialing(8057148453);
 
 
 
@@ -44,7 +45,13 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+function makeCounter() {
+  var counter = 0;
+  return function () {
+      return counter++;
+  }
+
+}
   var count = makeCounter();
   count() // 1
   count() // 2
